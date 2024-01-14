f3dee4de4796ef276c0218f3bd534e30ea441460
王春平 <2748721819@qq.com>
2024-01-14 23:37:01 +0800

    feat: 项目工程搭建

---
+++ src/main/java/com/lb/git/GitTest.java
@@ -0,0 +1,16 @@
+package com.lb.git;
+
+/**
+ * @author sb
+ */
+public class GitTest {
+
+    /**
+     * 主方法
+     *
+     * @param args 命令行参数
+     */
+    public static void main(String[] args) {
+        System.out.println("git test");
+    }
+}
